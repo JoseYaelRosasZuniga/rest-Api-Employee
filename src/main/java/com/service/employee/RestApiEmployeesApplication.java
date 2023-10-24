60d7ea0ad039100002a2c08f2cb86267a29ae6d8
JoseYaelRosasZuniga <zS22004542@estudiantes.uv.mx>
2023-10-23 22:22:50 -0600

    agregarndo entity, repository y properties

---
+++ src/main/java/com/service/employee/RestApiEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.service.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiEmployeesApplication.class, args);
+	}
+
+}
